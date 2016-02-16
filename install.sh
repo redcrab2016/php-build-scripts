a7e2f8ac3a2ab16df0623ca1cbebb30d36be545f
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-16 19:21:53 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+# ImagicalMine Installation Script for Mac OS X and Linux
+#  _    _            _    _______ 
+# | |  | |    /\    | |  |__   __|
+# | |__| |   /  \   | |     | |   
+# |  __  |  / /\ \  | |     | |   
+# | |  | | / ____ \ | |____ | | _ 
+# |_|  |_|/_/    \_\|______||_|(_)
+# 
+# This file is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+# Before you start doing anything, read the license for more detail into what you are allowed to do and not do.                             
+                                 
 clear
  echo "  _                       _           _ __  __ _             "
  echo " (_)                     (_)         | |  \/  (_)            "
